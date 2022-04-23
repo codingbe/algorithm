8b0d81f3121d7b3cb8a6c18be63aa2911c75fefd
codingbe <voluntad3000@gmail.com>
2022-04-24 01:30:59 +0900

    [Bronze III] Title: A+B - 8, Time: 120 ms, Memory: 9384 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync("./dev/stdin").toString().split("\n");
+const T = parseInt(input[0]);
+
+const answer = [];
+
+for (let i = 1; i <= T; i++) {
+   const [A, B] = input[i].split(" ").map((num) => parseInt(num));
+   answer.push(`Case #${i}: ${A} + ${B} = ${A + B}`);
+}
+
+console.log(answer.join("\n"));
