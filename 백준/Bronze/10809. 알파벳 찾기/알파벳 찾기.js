21eeac366ffb967ecab6d1e08f989d9eb58ae58f
codingbe <voluntad3000@gmail.com>
2022-05-01 21:04:20 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 136 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,32 @@
+const input = require("fs").readFileSync("./dev/stdin").toString();
+
+const answer = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+console.log(answer.map((alphabet) => input.indexOf(alphabet)).join(" "));
