250f1f39634138287e74e46c94f1b25420edf330
codingbe <voluntad3000@gmail.com>
2022-05-05 21:45:02 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
@@ -1,9 +1,14 @@
 const input = parseInt(require("fs").readFileSync("./dev/stdin").toString());
 
-const fibonacci = (num) => {
-  if (num === 0) return 0;
-  else if (num === 1) return 1;
-  return fibonacci(num - 1) + fibonacci(num - 2);
+const solution = (num) => {
+  const memo = {};
+  const fibonacci = (num) => {
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+    if (memo[num]) return memo[num];
+    return (memo[num] = fibonacci(num - 1) + fibonacci(num - 2));
+  };
+  return fibonacci(num);
 };
 
-console.log(fibonacci(input));
+console.log(solution(input));
