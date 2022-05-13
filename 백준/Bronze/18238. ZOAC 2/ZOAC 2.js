cd40962cc8dbe5225cac90f7b9d107d6e4f57792
codingbe <voluntad3000@gmail.com>
2022-05-13 16:18:36 +0900

    [Bronze II] Title: ZOAC 2, Time: 120 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/18238. ZOAC 2/ZOAC 2.js
@@ -0,0 +1,17 @@
+const input = require("fs")
+  .readFileSync("./dev/stdin")
+  .toString()
+  .trim()
+  .split("")
+  .map((char) => char.charCodeAt(0));
+
+let start = 65;
+
+console.log(
+  input.reduce((prev, curr) => {
+    const left = start - curr < 0 ? start - curr + 26 : start - curr;
+    const right = curr - start < 0 ? curr - start + 26 : curr - start;
+    start = curr;
+    return prev + Math.min(left, right);
+  }, 0)
+);
