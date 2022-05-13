4f907c8440e0a38579b84cf7f682c7c0ad9648da
codingbe <voluntad3000@gmail.com>
2022-05-13 13:49:55 +0900

    [Bronze II] Title: 욱제는 효도쟁이야!!, Time: 196 ms, Memory: 14772 KB -BaekjoonHub

--- 백준/Bronze/14487. 욱제는 효도쟁이야！！/욱제는 효도쟁이야！！.js
+++ 백준/Bronze/14487. 욱제는 효도쟁이야！！/욱제는 효도쟁이야！！.js
@@ -1,9 +1,4 @@
 let [N, I] = require("fs").readFileSync("./dev/stdin").toString().split("\n");
+I = I.split(" ").map((num) => parseInt(num));
 
-N = parseInt(N);
-I = I.split(" ")
-  .map((num) => parseInt(num))
-  .sort((a, b) => b - a)
-  .splice(1, N);
-
-process.stdout.write(`${I.reduce((prev, curr) => prev + curr, 0)}`);
+process.stdout.write(`${I.reduce((prev, curr) => prev + curr, 0) - Math.max(...I)}`);
