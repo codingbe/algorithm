678565d843ea9ce28ff83c1446b40c6686dcd0bf
codingbe <voluntad3000@gmail.com>
2022-07-13 03:31:05 +0900

    [Bronze II] Title: 수 정렬하기, Time: 128 ms, Memory: 9396 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -1,13 +1,10 @@
-let answer = "";
-
-require("fs")
-  .readFileSync("./dev/stdin")
-  .toString()
-  .trim()
-  .split("\n")
-  .slice(1)
-  .map((num) => parseInt(num))
-  .sort((a, b) => a - b)
-  .forEach((num) => (answer += `${num}\n`));
-
-console.log(answer);
+process.stdout.write(
+  require("fs")
+    .readFileSync("./dev/stdin")
+    .toString()
+    .trim()
+    .split("\n")
+    .slice(1)
+    .sort((a, b) => a - b)
+    .join("\n")
+);
