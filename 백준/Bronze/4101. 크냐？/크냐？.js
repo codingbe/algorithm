2618c2ff02ab8df6db9071b9f4d0e346869cb3c9
codingbe <voluntad3000@gmail.com>
2022-07-21 13:36:36 +0900

    [Bronze V] Title: 크냐?, Time: 120 ms, Memory: 9372 KB -BaekjoonHub

--- 백준/Bronze/4101. 크냐？/크냐？.js
+++ 백준/Bronze/4101. 크냐？/크냐？.js
@@ -1,11 +1,14 @@
-require("fs")
-  .readFileSync("./dev/stdin")
-  .toString()
-  .trim()
-  .split("\n")
-  .map((temp) => temp.split(" ").map((num) => parseInt(num)))
-  .forEach((nums, idx, arr) => {
-    const [A, B] = nums;
-    if (arr.length - 1 === idx) return;
-    console.log(A > B ? "Yes" : "No");
-  });
+console.log(
+  require("fs")
+    .readFileSync("./dev/stdin")
+    .toString()
+    .trim()
+    .split("\n")
+    .map((temp) => temp.split(" ").map((num) => parseInt(num)))
+    .reduce((prev, cur, idx, arr) => {
+      if (idx === arr.length - 1) return prev.trim();
+      const [A, B] = cur;
+      if (A > B) return prev + "Yes\n";
+      return prev + "No\n";
+    }, "")
+);
