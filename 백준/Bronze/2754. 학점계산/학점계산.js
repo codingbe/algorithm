c89e02f5f3398b4643ddad5c4f221de18657b04b
codingbe <voluntad3000@gmail.com>
2022-07-25 17:12:24 +0900

    [Bronze V] Title: 학점계산, Time: 112 ms, Memory: 9308 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.js
+++ 백준/Bronze/2754. 학점계산/학점계산.js
@@ -1,23 +1,38 @@
 const grade = require("fs").readFileSync("./dev/stdin").toString().trim();
 
-const grades = {
-  "A+": "4.3",
-  A0: "4.0",
-  "A-": "3.7",
+function calGrade(grade) {
+  switch (grade) {
+    case "A+":
+      return "4.3";
+    case "A0":
+      return "4.0";
+    case "A-":
+      return "3.7";
 
-  "B+": "3.3",
-  B0: "3.0",
-  "B-": "2.7",
+    case "B+":
+      return "3.3";
+    case "B0":
+      return "3.0";
+    case "B-":
+      return "2.7";
 
-  "C+": "2.3",
-  C0: "2.0",
-  "C-": "1.7",
+    case "C+":
+      return "2.3";
+    case "C0":
+      return "2.0";
+    case "C-":
+      return "1.7";
 
-  "D+": "1.3",
-  D0: "1.0",
-  "D-": "0.7",
+    case "D+":
+      return "1.3";
+    case "D0":
+      return "1.0";
+    case "D-":
+      return "0.7";
 
-  F: "0.0",
-};
+    case "F":
+      return "0.0";
+  }
+}
 
-process.stdout.write(`${grades[grade]}`);
+process.stdout.write(calGrade(grade));
