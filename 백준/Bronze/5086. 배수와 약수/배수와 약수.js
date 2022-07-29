0914430deb1d2c3d1e2c3ebe1429951ab44ee675
codingbe <voluntad3000@gmail.com>
2022-07-29 23:23:42 +0900

    [Bronze III] Title: 배수와 약수, Time: 116 ms, Memory: 9300 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.js
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.js
@@ -7,14 +7,12 @@ const input = require("fs")
 input.pop();
 
 function solution(input) {
-  return input
-    .reduce((answer, cur) => {
-      const [A, B] = cur;
-      if (A % B === 0) return [...answer, "multiple"];
-      if (A % B === A) return [...answer, "factor"];
-      return [...answer, "neither"];
-    }, [])
-    .join("\n");
+  return input.reduce((answer, cur) => {
+    const [A, B] = cur;
+    if (A % B === 0) return answer + "multiple\n";
+    if (A % B === A) return answer + "factor\n";
+    return answer + "neither\n";
+  }, "");
 }
 
 process.stdout.write(solution(input));
